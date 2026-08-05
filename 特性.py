2d469407e9d78c33da93dc86dfb6e5077f65bdc4
郁金香没me浴巾香 <1958797940@qq.com>
2026-08-05 20:29:25 +0800

    Add files via upload

--- 特性.py
+++ 特性.py
@@ -8,5 +8,25 @@ print(trim(' hfehfe   '))
 
 
 
+def findminandmax(L):
+    Max = L[0]
+    Min = L[0]
+    for i in L:
+        if i >Max:
+             Max = i 
+        if i < Min:
+                Min = i 
+    return(Max,Min)
+print (findminandmax([1,3,90,5,777,8,]))
+
+
+l1 =['Hello','World',18,'Apple']
+L2 =[s.lower() for s in l1 if isinstance(s,str)]
+print(L2)
+if L2 == ['hello', 'world', 'apple']:
+    print('测试通过!')
+else:
+    print('测试失败!')
+
 
 
