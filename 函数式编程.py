71d067b1ae7f528d7fd8e9b19b787af00b0e7a37
郁金香没me浴巾香 <1958797940@qq.com>
2026-08-10 16:49:16 +0800

    Add files via upload

--- 函数式编程.py
+++ 函数式编程.py
@@ -172,6 +172,52 @@ print(L)
 L = list(filter(lambda n: n%2==1, range(1, 20)))
 
 print(L)
+
 '''
+import functools
+def log(text):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kw):
+            print('%s %s():' % (text, func.__name__))
+            return func(*args, **kw)
+        return wrapper
+    return decorator
 
 
+@log('execute')
+def hel():
+    print("hello")
+
+f = hel
+f()
+print(f.__name__)
+
+
+
+import time, functools
+
+def metric(fn):
+    @functools.wraps(fn)
+    def wer(*arg,**kw):       
+        print('%s executed in %s ms' % (fn.__name__, 10.24))
+        return fn(*arg,**kw)
+    return wer
+
+# 测试
+@metric
+def fast(x, y):
+    time.sleep(0.0012)
+    return x + y;
+
+@metric
+def slow(x, y, z):
+    time.sleep(0.1234)
+    return x * y * z;
+
+f = fast(11, 22)
+s = slow(11, 22, 33)
+if f != 33:
+    print('测试失败!')
+elif s != 7986:
+    print('测试失败!')
